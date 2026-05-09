2ca9f37c333247097b842ad4f9fe1be5c1e05775
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-09 13:55:38 -0700

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,8 +1,12 @@
 fun main() {
   while (true) {
     print("$ ")
-    when (val line = readlnOrNull()) {
-      "exit" -> return
+    val line = readln()
+    val lineSplit = line.split(" ")
+                        .filter { it.isNotBlank() }
+    when {
+      lineSplit.firstOrNull() == "echo" -> println(lineSplit.drop(1).joinToString(" "))
+      lineSplit.firstOrNull() == "exit" -> return
       else -> println("$line: command not found")
     }
   }
