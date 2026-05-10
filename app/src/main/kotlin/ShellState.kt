397fe5f44002e714d5432d67146ae0c314fa037f
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-10 15:48:49 -0700

    codecrafters submit [skip ci]

--- app/src/main/kotlin/ShellState.kt
+++ app/src/main/kotlin/ShellState.kt
@@ -1,5 +1,5 @@
 class ShellState(
   var currentWorkingDirectory: String = System.getProperty("user.dir"),
-  var homeDirectory: String = System.getProperty("user.home")
+  var homeDirectory: String = System.getenv("HOME") ?: System.getProperty("user.home")
 )
 
