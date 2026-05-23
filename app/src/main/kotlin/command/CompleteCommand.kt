682c4e12f82596cccd6d433590fd084dd30955bb
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-22 17:25:55 -0700

    codecrafters submit [skip ci]

--- app/src/main/kotlin/command/CompleteCommand.kt
+++ app/src/main/kotlin/command/CompleteCommand.kt
@@ -2,5 +2,12 @@ package command
 
 class CompleteCommand : Command {
   override val text = "complete"
-  override fun execute(name: String, args: List<String>): ExecutionResult = ExecutionResult()
+  override fun execute(name: String, args: List<String>): ExecutionResult {
+    if (args.firstOrNull() == "-p") {
+      // lookup builtin commands
+      return ExecutionResult(stderr = "complete: ${args.getOrNull(1)}: no completion specification")
+    }
+
+    return ExecutionResult()
+  }
 }
