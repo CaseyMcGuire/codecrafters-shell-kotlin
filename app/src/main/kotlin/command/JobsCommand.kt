ca80e2a71f88513a26d85ea04abf5daefe7d34af
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-25 12:28:21 -0700

    codecrafters submit [skip ci]

--- app/src/main/kotlin/command/JobsCommand.kt
+++ app/src/main/kotlin/command/JobsCommand.kt
@@ -6,14 +6,20 @@ class JobsCommand(private val shellState: ShellState) : Command {
   override val text: String = "jobs"
   override fun execute(name: String, args: List<String>): ExecutionResult {
     val lines = mutableListOf<String>()
-    for (entry in shellState.jobNumberToProcess) {
+    val processes = shellState.jobNumberToProcess.values.sortedBy { it.jobNumber }
+    for (processState in processes) {
       val builder = StringBuilder()
-      val processState = entry.value
       builder.append("[${processState.jobNumber}]")
       if (shellState.currentJobNumber.get() == processState.jobNumber) {
         builder.append("+")
       }
-      builder.append("  ")
+      else if (shellState.currentJobNumber.get() - 1 == processState.jobNumber) {
+        builder.append("-")
+      }
+      else {
+        builder.append(" ")
+      }
+      builder.append(" ")
       val padding = 24 - processState.status.name.length
       builder.append(processState.status.name.padEnd(padding))
       builder.append(processState.command)
