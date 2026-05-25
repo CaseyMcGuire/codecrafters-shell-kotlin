4e50a6b9bbe0d90475c9970c84d6482d0eec3b8e
CaseyMcGuire <caseyjaymcguire@gmail.com>
2026-05-25 13:59:10 -0700

    codecrafters submit [skip ci]

--- app/src/main/kotlin/command/JobsCommand.kt
+++ app/src/main/kotlin/command/JobsCommand.kt
@@ -10,18 +10,19 @@ class JobsCommand(private val shellState: ShellState) : Command {
     for (processState in processes) {
       val builder = StringBuilder()
       builder.append("[${processState.jobNumber}]")
-      if (shellState.currentJobNumber.get() == processState.jobNumber) {
-        builder.append("+")
-      }
-      else if (shellState.currentJobNumber.get() - 1 == processState.jobNumber) {
-        builder.append("-")
-      }
-      else {
-        builder.append(" ")
+      when (processState.jobNumber) {
+        shellState.currentJobNumber.get() -> {
+          builder.append("+")
+        }
+        shellState.currentJobNumber.get() - 1 -> {
+          builder.append("-")
+        }
+        else -> {
+          builder.append(" ")
+        }
       }
       builder.append(" ")
-      val padding = 24 - processState.status.name.length
-      builder.append(processState.status.name.padEnd(padding))
+      builder.append(processState.status.name.padEnd(24))
       builder.append(processState.command)
       lines.add(builder.toString())
     }
